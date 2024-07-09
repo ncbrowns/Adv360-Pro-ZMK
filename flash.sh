d465707d5f7b0bc16c1e5f5295870cd1fdacfc66
Pat Brown <pbrown@nvidia.com>
2024-07-08 23:53:07 -0400

    Add flashing infrastructure.

---
+++ flash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ $# -ne 1 ]] ; then
+  echo "Must provide one argument - 'left' or 'right'."
+  exit 1
+fi
+side=$1
+sudo mount -t drvfs D: /mnt/d
+if ! ls -1 /mnt/d | egrep -i "^current.uf2$" ; then
+  echo "Couldn't find 'current.uf2' for $side side."
+  exit 1
+fi
+if ! cp $side.uf2 /mnt/d ; then
+  echo "Couldn't copy '$side.uf2' to target drive."
+  exit 1
+fi
+exit 0
